421951c1f8cbf2ff2e183b59427b5b10c70fb7c4
Luis Americo Auyadermont <lauyader2011@gmail.com>
2014-12-02 08:10:03 -0430

    Actualizada la carpeta SACANotas del sistema principal

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ SACANotasV0.1
 =============
 
 Version 0.1 Inicio con PhoneGap y Jquery Mobil
+Agregado la carpeta del sistema central
--- www/js/main.js
+++ www/js/main.js
@@ -41,3 +41,60 @@ function menuPulsado()
 function busquedaPulsado()
 {
 }
+
+// Base de datos de videobrain
+
+var existe_db
+var db
+
+function onDeviceReady(){
+
+	mkLog("Aplicación Cargadan  y Lista");
+
+	existe_db = window.localStorage.getItem("existe_db");
+
+	db = window.localStorage("agenda_curso"), "1.0", "Db deñ Curso", 200000);
+
+	if(existe_db == null){
+
+   		creaDB)();
+
+	}
+
+
+	window.localStorage.getItem("existe_db", 1);
+}
+
+function creaDB(){
+	db.transaction(creaNuevaDB, errorDB, creaSuccess);
+	//window.localStorage.setItem("existe_db", 1);
+
+}
+
+function creaNuevaDb(tx){
+	mklog("Crear base de datos");
+
+	tx.executeSql('DROP TABLET IF EXISTS agenda_curso');
+
+	var sql = "CREATE TABLET IF EXISTS agenda_curso ( "+
+		"id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+		"nombre VARCHAR(50)," +
+		"apellidos VARCHAR(50)," +
+		"telefono VARCHAR(30)," +
+		"categoria VARCHAR(30)," +
+		"foto VARCHAR(200)," +
+		"email VARCHAR(30) )";
+	tx.executeSql(sql);
+
+	txt.executeSql("INSERT INTO agenda_curso(id,nombre,apellidos,telefono,categoria,foto,email) VALUES (1,'Jose','Perez', '+58246224264','amigo','lauyader@gmail.com'");
+
+}
+
+function creaSucess(){
+	window.localStorage.setItem("existe_db", 1);
+}
+
+function errorDB(err){
+	mkLog("Error procesando SQL" +err.code);
+	navigator.notification.alert("Error procesando SQL" +err.code);
+}
